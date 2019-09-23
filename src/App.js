06357779b09005ef6d66e33627c9205024c9f094
onyeka mmakwe <notmaks@yahoo.com>
2019-09-23 23:44:03 +0100

    Setup react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import Homepage from './pages/homepage/homepage'
+import React from "react";
+import Homepage from "./pages/homepage/homepage";
+import { Switch, Route } from "react-router-dom";
+
+const Hats = props => {
+  console.log(props);
+  return <h1>Hats {props.match.params.hatid}</h1>;
+};
 
 function App() {
   return (
-    <>
-    <Homepage/>
-    </>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/hats" component={Hats} />
+        <Route path="/hats/:hatid" component={Hats} />
+      </Switch>
+    </div>
   );
 }
 
